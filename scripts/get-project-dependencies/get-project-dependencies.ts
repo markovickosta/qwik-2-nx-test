f88b042977492a1370aeb329818f73eba5cf5999
markovickosta <markovic.kosta@outlook.com>
2025-01-16 19:40:26 +0100

    Pretty

--- scripts/get-project-dependencies/get-project-dependencies.ts
+++ scripts/get-project-dependencies/get-project-dependencies.ts
@@ -1,7 +1,7 @@
 const globs = require('./output.json');
 
 const result = Object.values(globs.graph.nodes).map((node: any) => {
-  return `../../${node.data.root}/src`;
+	return `../../${node.data.root}/src`;
 });
 
 process.stdout.write(result.toString());
